a23f9398fc60e7999a8755b5fb1c22df10b0769a
gyubong_park <gyubong_park@tmaxsoft.com>
2026-01-06 17:21:56 +0900

    update mini-lb example: refine `real_setup.sh` script and improve echo server implementation

--- mini-lb/setup/echo.py
+++ mini-lb/setup/echo.py
@@ -1,17 +1,21 @@
-# Echo server program
 import socket
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+HOST = ''
+PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 포트 재사용 옵션 (재시작 시 에러 방지)
 s.bind((HOST, PORT))
 s.listen(1)
-print (' listening')
+print('Listening on port', PORT)
 
-conn, addr = s.accept()
-print ('Connected by' + addr)
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+# 무한 루프를 통해 계속 연결을 받도록 수정
+while True:
+    conn, addr = s.accept()
+    # 쉼표(,)를 사용하면 튜플도 출력 가능
+    print('Connected by', addr)
+
+    with conn: # with문을 쓰면 연결이 끝나면 알아서 close됨
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
--- mini-lb/setup/real_setup.sh
+++ mini-lb/setup/real_setup.sh
@@ -5,7 +5,7 @@ ip route del default
 ip route add default via 10.111.222.12
 
 # The vip is assigned to lo
-ip addr add 192.168.10.1 dev lo
+ip addr add 192.168.10.1/32 dev lo
 
 ip link add name ipip0 type ipip external
 ip link set up dev ipip0
@@ -15,5 +15,5 @@ ip a a 127.0.0.42/32 dev ipip0
 sysctl -w net.ipv4.conf.all.rp_filter=0
 
 # echo server
-python /data/echo.py
+python3 /data/echo.py
 tail -f /dev/null
